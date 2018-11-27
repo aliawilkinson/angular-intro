c778fe1023090837646c8ebf2c7c9fdc73a667b0
Alia Wilkinson <aliawilkinson@gmail.com>
2018-11-27 15:06:46 -0800

    added customers-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './customers/customers.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomersComponent
+    CustomersComponent,
+    CustomersListComponent
   ],
   imports: [
     BrowserModule,
